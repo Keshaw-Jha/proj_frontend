62a92e5ae30c4f61d34cdac4c1b641c9698a9648
KeshawNGlo <keshaw.jha@natureglobal.com>
2024-03-27 12:09:01 +0530

    card enhanced

---
+++ src/components/dashboardCard.tsx
@@ -0,0 +1,20 @@
+type DashboardCardProps = {
+  icon?: JSX.Element;
+  title: string;
+  value: string;
+};
+
+// Reusable SidebarItem component
+function DashboardCard({ title, value, icon }: DashboardCardProps) {
+  return (
+    <div className=" h-[150px] bg-[#ffc9af] rounded-lg p-6 grid grid-cols-3 text-[#FF204E]">
+      <div className="grid grid-rows-2 col-span-2">
+        <div className="text-2xl font-bold">{title}</div>
+        <div className="text-5xl font-bold">{value}</div>
+      </div>
+      <div className="flex justify-center items-center">{icon}</div>
+    </div>
+  );
+}
+
+export default DashboardCard;
--- src/pages/Dashboard/dashboard-page.tsx
+++ src/pages/Dashboard/dashboard-page.tsx
@@ -1,28 +1,27 @@
-import React from "react";
-
-type DashboardCardProps = {
-  icon?: JSX.Element;
-  title: string;
-  value: string;
-};
-
-// Reusable SidebarItem component
-function DashboardCard({ title, value }: DashboardCardProps) {
-  return (
-    <div className=" h-[150px] bg-[#ffc9af] rounded-xl p-5 grid grid-rows-2 text-[#FF204E]">
-      <div className="text-2xl font-bold">{title}</div>
-      <div className="text-5xl font-bold">{value}</div>
-    </div>
-  );
-}
+import DashboardCard from "../../components/dashboardCard";
+import BookOnlineIcon from "@mui/icons-material/BookOnline";
+import GroupsIcon from "@mui/icons-material/Groups";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
 function DashboardPage() {
   return (
     <div className="flex flex-col">
       <div className="grid md:grid-cols-3 gap-10">
-        <DashboardCard title={"Ticket's Booked"} value={"1.5k"} />
-        <DashboardCard title={"Active Count"} value={"250"} />
-        <DashboardCard title={"Exited"} value={"750"} />
+        <DashboardCard
+          title={"Ticket's Booked"}
+          value={"1.5k"}
+          icon={<BookOnlineIcon className="!text-8xl" />}
+        />
+        <DashboardCard
+          title={"Active Count"}
+          value={"250"}
+          icon={<GroupsIcon className="!text-8xl" />}
+        />
+        <DashboardCard
+          title={"Total Exit's"}
+          value={"750"}
+          icon={<ExitToAppIcon className="!text-8xl" />}
+        />
       </div>
       <div className="flex-1"></div>
     </div>
