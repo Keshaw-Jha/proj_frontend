9a22cb477d6be50100b78840b0752b5509c40b3c
Keshaw-Jha <78222085+Keshaw-Jha@users.noreply.github.com>
2024-04-13 23:30:57 +0530

    route adjusted

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const handleButtonCLick = () => {
+    navigate("/admin");
+  };
+
   return (
     <AppBar position="static" className="!bg-[#FF204E] h-20 pt-2 z-10">
       <Toolbar className="flex items-center justify-center gap-4">
@@ -11,6 +18,12 @@ const Header = () => {
           style={{ fontFamily: "yatra one" }}>
           प्रVEश SYस्टUM
         </h3>
+        <Button
+          color={"warning"}
+          className="!bg-orange-500 !text-white "
+          onClick={handleButtonCLick}>
+          Admin
+        </Button>
       </Toolbar>
     </AppBar>
   );
