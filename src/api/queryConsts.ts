2f8c201e1d55742e9cc8b397153466637e48290b
Keshaw-Jha <78222085+Keshaw-Jha@users.noreply.github.com>
2024-04-13 23:09:30 +0530

    endpoint updated

--- src/api/queryConsts.ts
+++ src/api/queryConsts.ts
@@ -1,10 +1,10 @@
 export const base_url = process.env.API_URL;
 
 export const QueryConst = {
-  submitForm: `${base_url}/submitform`,
-  submitOtp: `${base_url}/submitotp`,
-  getQr: `${base_url}/getqr`,
-  getAllTickets: `${base_url}/gettickets`,
-  getStats: `${base_url}/getstats`,
-  updateTicketStatus: `${base_url}/updateticketstatus`,
+  submitForm: `${base_url}submitform`,
+  submitOtp: `${base_url}submitotp`,
+  getQr: `${base_url}getqr`,
+  getAllTickets: `${base_url}gettickets`,
+  getStats: `${base_url}getstats`,
+  updateTicketStatus: `${base_url}updateticketstatus`,
 };
