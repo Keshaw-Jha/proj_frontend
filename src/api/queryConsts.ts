01433b541f7f4ddfaa489518c46855c59e2d7302
Keshaw-Jha <78222085+Keshaw-Jha@users.noreply.github.com>
2024-05-12 00:16:51 +0530

    changes

--- src/api/queryConsts.ts
+++ src/api/queryConsts.ts
@@ -7,6 +7,6 @@ export const QueryConst = {
   getAllTickets: `${base_url}gettickets`,
   getStats: `${base_url}getstats`,
   updateTicketStatus: `${base_url}updateticketstatus`,
-  logIn: `${base_url}logIn`,
-  signIn: `${base_url}signIn`,
+  logIn: `${base_url}login`,
+  signIn: `${base_url}signin`,
 };
